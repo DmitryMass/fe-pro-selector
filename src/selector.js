6c4570ac08901014d2a5bc8c83bd011ee1710be2
Dmitry Mass <dimamikron41@gmail.com>
2021-12-28 21:36:08 +0300

    fnc const select

--- src/selector.js
+++ src/selector.js
@@ -6,7 +6,7 @@ const obj = {
   },
 };
 
-function selector(obj, keys) {
+const selector = (obj, keys) => {
   // Ваше решение должно быть здесь
   let curObj = obj;
   keys.forEach(function (item) {
@@ -17,7 +17,7 @@ function selector(obj, keys) {
     }
   });
   return curObj;
-}
+};
 console.log(selector(obj, ["test", "myField", "value string"])); // => 'result'
 console.log(selector(obj, ["test", "notExistField", "value string"])); // => ''
 
